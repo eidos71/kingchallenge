de5b2a2502c1caed7bd0c8e931febe0151657def
eidos71 <eidos33@gmail.com>
2014-09-12 13:38:27 +0200

    Added Test for persistance.

--- src/test/java/test/eidos/kingchanllenge/persistance/TestLoginPersistance.java
+++ src/test/java/test/eidos/kingchanllenge/persistance/TestLoginPersistance.java
@@ -3,6 +3,9 @@ package test.eidos.kingchanllenge.persistance;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.easymock.EasyMockRunner;
 import org.easymock.EasyMockSupport;
 import org.eidos.kingchallenge.exceptions.LogicKingChallengeException;
@@ -10,6 +13,7 @@ import org.eidos.kingchallenge.model.KingUser;
 import org.eidos.kingchallenge.persistance.LoginPersistanceMap;
 import org.eidos.kingchallenge.persistance.SimpleLoginPersistanceMap;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -20,7 +24,14 @@ public class TestLoginPersistance  extends EasyMockSupport  {
 			.getLogger(TestLoginPersistance.class);
 	private static final LoginPersistanceMap<Long, String, KingUser> bag = new SimpleLoginPersistanceMap();
 	private KingUser aKingUser=null;
-	
+	@Before
+	public void setup() {
+		//Make sure the map is clean at the start of each of this Test
+		Map<Long, KingUser> result = bag.getMapByLogin();
+		for (Entry<Long, KingUser> entry : result.entrySet() ){
+			bag.removeByLogin(entry.getKey());
+		}
+	}
 	/**
 	 * Inserts an element in bag
 	 */
@@ -33,7 +44,33 @@ public class TestLoginPersistance  extends EasyMockSupport  {
 	}
 	
 	@Test (expected=LogicKingChallengeException.class)
-	public void testInsertWrongUserId() {
+	public void testInsertLoginIDasZero() {
 		aKingUser =new KingUser.Builder(0).build();
+	}
+	@Test (expected=LogicKingChallengeException.class)
+	public void testInsertLoginIDNegative() {
+		aKingUser =new KingUser.Builder(-132).build();
+	}
+	@Test  (expected=AssertionError.class)
+	public void testInsertDuplicateIdStoresfistEntry() {
+		aKingUser =new KingUser.Builder(1).build();
+		bag.put(aKingUser.getKingUserId().get(), aKingUser.getSessionKey(), aKingUser);
+		LOG.debug("{} ",aKingUser);
+		KingUser secondUser = new KingUser.Builder(1).build();
+		bag.put(aKingUser.getKingUserId().get(), aKingUser.getSessionKey(), secondUser);
+		LOG.debug("{} ",secondUser);
+		 assertThat("T", bag.getMapByLogin().get(1L).getSessionKey() , equalTo(aKingUser.getSessionKey()));
+
+	}
+	@Test
+	public void testInsertDuplicateIDStoresLastEntry() {
+		aKingUser =new KingUser.Builder(1).build();
+		bag.put(aKingUser.getKingUserId().get(), aKingUser.getSessionKey(), aKingUser);
+		LOG.debug("{} ",aKingUser);
+		aKingUser =new KingUser.Builder(1).build();
+		bag.put(aKingUser.getKingUserId().get(), aKingUser.getSessionKey(), aKingUser);
+		LOG.debug("{} ",aKingUser);
+		 assertThat("Total King users 1", bag.getMapByLogin().get(1L).getSessionKey() , equalTo(aKingUser.getSessionKey()));
+
 	}
 }
